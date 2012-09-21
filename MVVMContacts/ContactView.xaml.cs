541ab7e9a8906858aacfacc1a25cf9df64c9b1b0
Onur Baykal <theaob@gmail.com>
2012-09-21 15:22:17 +0300

    added mainview and contactview

---
+++ MVVMContacts/ContactView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Apex.MVVM;
+
+namespace MVVMContacts
+{
+    /// <summary>
+    /// Interaction logic for ContactView.xaml
+    /// </summary>
+    [View(typeof(object))]
+    public partial class ContactView : UserControl
+    {
+        public ContactView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMContacts/MainView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Apex.MVVM;
+
+namespace MVVMContacts
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    [View(typeof(MainViewModel))]
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
